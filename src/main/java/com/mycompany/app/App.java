9be5be33efe48c331f32a059fd5aeaa62c80a683
OguzhanGuldamlasi <oguldamlasi@etu.edu.tr>
2019-01-31 21:04:05 +0300

    First Commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,33 +12,24 @@ public class App
 
 
     }
-    public static boolean searchInTwoArrays(ArrayList<Integer> array1,ArrayList<Integer>array2,int elem1,int elem2) {
-        if(elem1!=elem2)
+    public static boolean searchInTwoArrays(ArrayList<Integer> array1,ArrayList<Integer>array2,String str1,String str2) {
+        if(array1==null||array2==null||str1==null||str2==null)
             return false;
-        if (array1 == null||array1.size()==0)
+        if(str1.length()==0||str2.length()==0)
             return false;
-        if (array2 == null||array2.size()==0)
+        if(array1.size()>str1.length()||array2.size()>str2.length())
             return false;
-        int index1=Integer.MIN_VALUE;
-        for (int i=0;i<array1.size();++i) {
-            if (array1.get(i) == elem1){
-                index1=i;
-                break;
+        if(array1.size()==0||array2.size()==0)
+            return false;
+        int indexNum=array1.size();
+        for (int i=0;i<indexNum;++i) {
+            char charFromStr1AtIndexI=str1.charAt(array1.get(i));
+            char charFromStr2AtIndexI=str2.charAt(array2.get(i));
+            if(charFromStr1AtIndexI!=charFromStr2AtIndexI){
+                return false;
             }
         }
-        if(index1==Integer.MIN_VALUE){//could not find elem1
-            return false;
-        }
-        int index2=Integer.MIN_VALUE;
-        for (int i=0;i<array2.size();++i) {
-            if (array1.get(i) == elem2){
-                index2=i;
-                break;
-            }
-        }
-        if(index2==Integer.MIN_VALUE){//could not find elem2
-            return false;
-        }
-        return index1 == index2;
+        return true;
+
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -12,28 +12,34 @@ public class AppTest
     extends TestCase
 {
 public void testFound() {
-        ArrayList<Integer> array1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
-        ArrayList<Integer> array2 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
-        assertTrue(new App().searchInTwoArrays(array1,array2, 2,2));
+        ArrayList<Integer> array1 = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3));
+        ArrayList<Integer> array2 = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3));
+        assertTrue(new App().searchInTwoArrays(array1,array2, "ahmet","ahmet"));
     }
 
     public void testNotFound() {
         ArrayList<Integer> array1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
         ArrayList<Integer> array2 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
-        assertFalse(new App().searchInTwoArrays(array1,array2, 5,6));
+        assertFalse(new App().searchInTwoArrays(array1,array2, "ahmet","kemal"));
     }
 
     public void testEmptyArray() {
         ArrayList<Integer> array = new ArrayList<Integer>();
-        assertFalse(new App().searchInTwoArrays(array,array, 1,123));
+        assertFalse(new App().searchInTwoArrays(array,array, "",""));
     }
 
     public void testNull() {
-        assertFalse(new App().searchInTwoArrays(null,null, 1,5));
+        assertFalse(new App().searchInTwoArrays(null,null, null,null));
     }
     public void testDiffrentIndex(){
-        ArrayList<Integer> array1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
-        ArrayList<Integer> array2 = new ArrayList<Integer>(Arrays.asList(1, 3, 2, 4));
-        assertEquals(false,new App().searchInTwoArrays(array1,array2, 2,3));
+        ArrayList<Integer> array1 = new ArrayList<Integer>(Arrays.asList(4, 2, 1, 0));
+        ArrayList<Integer> array2 = new ArrayList<Integer>(Arrays.asList(1, 3, 4, 2));
+        assertEquals(false,new App().searchInTwoArrays(array1,array2, "ahmet","ahmet"));
+    }
+    public void testSmallStrLength(){
+        ArrayList<Integer> array1 = new ArrayList<Integer>(Arrays.asList(4, 2, 1, 0));
+        ArrayList<Integer> array2 = new ArrayList<Integer>(Arrays.asList(1, 3, 4, 2));
+        assertEquals(false,new App().searchInTwoArrays(array1,array2, "ali","veli"));
+
     }
 }
